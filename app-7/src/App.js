e37e2f01f3760b816e7f9ed0c51dae8af3dafa0f
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-07-09 19:59:22 -0600

    a

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
+export default class App extends Component {
+constructor() {
+  super()
+  this.state = {
+    list: ``,
+    array: []
+  }
+}
+addToList(list) {
+  let newArray = [...this.state.array]
+  newArray.push(list)
+  this.setState({
+    array: newArray
+  })
+}
+handleChange(e) {
+  this.setState({
+    list: e.target.value})}
+    clear() {
+      this.setState({
+        array: []
+      })
+    }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <input onChange={e => {this.handleChange(e)}}/>
+       <button onClick={()=> this.addToList(this.state.list)}>Add To List</button>
+       <button onClick ={()=> this.clear()}>Clear</button>
+       <h4>{this.state.array.map(e => <h4>{e}</h4>)}</h4>
       </div>
     );
   }
 }
 
-export default App;
